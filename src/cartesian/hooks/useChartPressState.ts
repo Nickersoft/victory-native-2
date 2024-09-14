bd191f269d21365c6d65d4c03bfa0800ffb3c131
Tyler Nickerson <nickersoft@gmail.com>
2024-09-14 13:30:15 -0700

    add dependencies to useIsPressActive

--- src/cartesian/hooks/useChartPressState.ts
+++ src/cartesian/hooks/useChartPressState.ts
@@ -60,6 +60,7 @@ const useIsPressActive = <Init extends ChartPressStateInit>(
     (val, oldVal) => {
       if (val !== oldVal) runOnJS(setIsPressActive)(val);
     },
+    [value.isActive],
   );
 
   return isPressActive;
